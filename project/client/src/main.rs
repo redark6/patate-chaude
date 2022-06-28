beeac73314530c912c3840984dd543078ac0c29e
redark6 <redark66@gmail.com>
2022-06-28 15:24:24 +0200

    ex of client

--- project/client/src/main.rs
+++ project/client/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    // // let message = Welcome { version: 1 };
+    // // let message = Message { welcome: Welcome { version: 1 }};
+    // let message = Message::Welcome(Welcome { version: 1 });
+    //
+    // let serialized = serde_json::to_string(&message);
+    // let serialized =
+    //     match serialized {
+    //         Ok(s) => s,
+    //         Err(err) => panic!("Ça s'est pas bien passé: {err}")
+    //     };
+    // // let serialized = serialized.unwrap();
+    // // let serialized = serialized.expect("Error");
+    // // let serialized = serialized?; => try operator
+
+    let stream = std::net::TcpStream::connect("localhost:7676");
+    match stream {
+        Ok(mut stream ) => {
+            let message = "Hello😇";
+            stream.write_all(&message.as_bytes()).unwrap();
+        }
+        Err(err) => panic!("Cannot connect: {err}")
+    }
+
+    // println!("{serialized}");
 }
