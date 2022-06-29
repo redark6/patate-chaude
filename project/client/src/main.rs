c8b888686419b00f66df4eb9c3ff32fa4cd68da9
rckam <rckmiri@gmail.com>
2022-06-29 10:35:46 +0200

    working write and read from server

--- project/client/src/main.rs
+++ project/client/src/main.rs
@@ -3,43 +3,42 @@ use std::net::TcpStream;
 use std::io::Write;
 use std::io::Read;
 
-fn messages(mut stream: TcpStream) {
+
+fn to_send_messsage_size(message: &str) -> [u8; 4]{
+    let u32_message_size: u32 = message.len().try_into().unwrap();
+    return u32_message_size.to_be_bytes();
+
+}
+
+fn messages(mut stream: TcpStream, message: &str) {
+    const MESSAGE_PREFIX_SIZE: usize = 4;
     let mut count = 0;
 
-    let message = "\"Hello\"";
-    let u32_message_size: u32 = message.len().try_into().unwrap();
-    let bytes = u32_message_size.to_be_bytes();
-    const MESSAGE_SIZE: usize = 4;
-
-    stream.write(&bytes);
+    stream.write(&to_send_messsage_size(message));
     stream.write(message.as_bytes());
 
+    let mut message_prefix_bytes = [0u8; MESSAGE_PREFIX_SIZE];
+    stream.read(&mut message_prefix_bytes);
+    let received_message_size = u32::from_be_bytes(message_prefix_bytes);
+    println!("{}", received_message_size);
 
-    loop {
-        let mut rx_bytes = [0u8; MESSAGE_SIZE];
-        // Read from the current data in the TcpStream
-        stream.read(&mut rx_bytes);
-        let received = std::str::from_utf8(&rx_bytes).expect("valid utf8");
-        println!("{}", received);
+    let message_size: usize = received_message_size.try_into().unwrap();
 
-
-        count += 1;
-        if count == 50 {
-            println!("Je meur");
-
-            break;
-        }
-    }
+    let mut message_bytes = vec![0u8; message_size];
+    stream.read(&mut message_bytes);
+    let received_message =  std::str::from_utf8(&message_bytes).expect("valid utf8");
+    println!("{}", received_message);
 }
 
 fn main() {
 
+    let message = "\"Hello\"";
 
     println!("Tentative de connexion au serveur...");
     match TcpStream::connect("127.0.0.1:7878") {
         Ok(stream) => {
             println!("Connexion au serveur réussie !");
-            messages(stream);
+            messages(stream, message);
         }
         Err(e) => {
             println!("La connexion au serveur a échoué : {}", e);
