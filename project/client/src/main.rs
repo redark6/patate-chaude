8feb1e89f58b97e6c5d5268fc4a25126074b83fb
rckam <rckmiri@gmail.com>
2022-06-30 11:05:41 +0200

    working multiple uses of stream (still wip)

--- project/client/src/main.rs
+++ project/client/src/main.rs
@@ -10,7 +10,7 @@ fn to_send_messsage_size(message: &str) -> [u8; 4]{
 
 }
 
-fn messages(mut stream: TcpStream, message: &str) {
+fn messages(mut stream: TcpStream, message: &str) -> String {
     const MESSAGE_PREFIX_SIZE: usize = 4;
 
     stream.write(&to_send_messsage_size(message)).ok();
@@ -19,25 +19,29 @@ fn messages(mut stream: TcpStream, message: &str) {
     let mut message_prefix_bytes = [0u8; MESSAGE_PREFIX_SIZE];
     stream.read(&mut message_prefix_bytes).ok();
     let received_message_size = u32::from_be_bytes(message_prefix_bytes);
-    println!("{}", received_message_size);
 
     let message_size: usize = received_message_size.try_into().unwrap();
 
     let mut message_bytes = vec![0u8; message_size];
     stream.read(&mut message_bytes).ok();
     let received_message =  std::str::from_utf8(&message_bytes).expect("valid utf8");
-    println!("{}", received_message);
+
+    return received_message.to_string();
 }
+
+
 
 fn main() {
 
     let message = "\"Hello\"";
+    let message2 = "{\"Subscribe\":{\"name\":\"free_patato2\"}}";
 
     println!("Tentative de connexion au serveur...");
     match TcpStream::connect("127.0.0.1:7878") {
         Ok(stream) => {
             println!("Connexion au serveur réussie !");
-            messages(stream, message);
+            println!("{}", messages(stream.try_clone().unwrap(), message));
+            println!("{}", messages(stream.try_clone().unwrap(), message2));
         }
         Err(e) => {
             println!("La connexion au serveur a échoué : {}", e);
